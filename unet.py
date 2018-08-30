8d0fcfaf0f052bbc186508642bd18b3b156acf4e
kpasad <kpasad@gmail.com>
2018-08-30 12:29:04 -0700

    Updated architecture.
    1. We seemed were short of one down layer. Drawing out the architecture and calulating the IP/OP tensor sizes of down_layers and up_layers showed that the implementation is correct.
    2. The problem is that concatanation with down layers happens with down_layer tensors before pooling.
    3. So we make modifcation to multi_conv and return both before and after pool tensors. We use pre-pool tensors from down_layer to concat with up layers.
    4. This allows us to increase the up_layers by one and output of up_layers is same as input image.

--- unet.py
+++ unet.py
@@ -20,7 +20,10 @@ class layer_params:
         self.layer_cnt=0
         self.logdir='./logdir'
         self.debug_level=2
-        self.pool=True
+        self.pool = True
+
+
+
 
 def multi_conv(input,prm):
 
@@ -43,8 +46,8 @@ def multi_conv(input,prm):
         if prm.pool == False:
             return conv[-1]
         pool = tf.layers.max_pooling2d(
-                conv[-1], (2, 2), strides=(2, 2), name="pool_{}".format(prm.layer_cnt))
-        return pool
+            conv[-1], (2, 2), strides=(2, 2), name="pool_{}".format(prm.layer_cnt))
+        return conv[-1], pool
 def up_multi_conv(down_layer,up_layer,prm):
     with tf.name_scope("up_layer_{}".format(prm.layer_cnt)):
         with tf.variable_scope("up_layer_{}".format(prm.layer_cnt)):
@@ -60,12 +63,10 @@ def up_multi_conv(down_layer,up_layer,prm):
                 concat_layer = tf.concat([up_conv_prev_layer,down_layer],axis=-1,name ="concat_".format(prm.layer_cnt))
 
     up_conv = multi_conv(concat_layer,prm)
-
-    if prm.debug_level == 2:
-        up_conv = tf.Print(up_conv, [], "Up layer,    Upsampled,    down_layer,    concat,     conv_concat ")
-        up_conv = tf.Print(up_conv,
-                           [tf.shape(up_layer), tf.shape(up_conv_prev_layer), tf.shape(down_layer),
-                            tf.shape(concat_layer), tf.shape(up_conv)])
+    # if prm.debug_level == 2:
+    #     up_conv = tf.Print(up_conv, [], "Up layer,    Upsampled,    down_layer,    concat,     conv_concat ")
+    #     up_conv = tf.Print(up_conv,
+    #                                   [tf.shape(up_layer), tf.shape(up_conv_prev_layer), tf.shape(down_layer),tf.shape(concat_layer), tf.shape(up_conv)])
 
     return up_conv
 
@@ -74,31 +75,37 @@ def make_unet_v1(X,prm):
     dims = tf.shape(X)
     down_resol_thresh=4
     next_layer_ip = X
-    down_layers=[]
+    down_layers_conv=[]
+    down_layers_pool = []
     up_layers = []
     for layer_cnt in range(0,prm.num_down_layers):
         print(" creating layer{}".format(layer_cnt))
         prm.layer_cnt = layer_cnt
-        down_layers.append(multi_conv(next_layer_ip,prm))
-        next_layer_ip = down_layers[-1]
-    prm.pool= False
-    for layer_cnt in range(0,prm.num_down_layers-1):
+        conv,pool = multi_conv(next_layer_ip,prm)
+        down_layers_conv.append(conv)
+        down_layers_pool.append(pool)
+        next_layer_ip = down_layers_pool[-1]
+    #print(down_layers)
+    prm.pool=False # For the upo layers, no pooling
+
+    for layer_cnt in range(0,prm.num_down_layers):
         print(" Up layers",layer_cnt)
         prm.layer_cnt = layer_cnt+prm.num_down_layers
-        next_layer_ip = up_multi_conv(down_layers[prm.num_down_layers-(2+layer_cnt)],next_layer_ip, prm)
-        up_layers.append(next_layer_ip)
-    return down_layers,up_layers
+        next_layer_ip=tf.Print(next_layer_ip,[tf.shape(next_layer_ip)])
+        up_layers.append(up_multi_conv(down_layers_conv[prm.num_down_layers-(1+layer_cnt)],next_layer_ip, prm))
+        next_layer_ip= up_layers[-1]
+    return down_layers_pool,up_layers,next_layer_ip
 
 prm = layer_params()
 X= tf.placeholder(tf.float32, (prm.batch_size, prm.ip_h, prm.ip_w, prm.n_chan))
-down_layers,up_layers = make_unet_v1(X,prm)
+down_layers,up_layers,final_out = make_unet_v1(X,prm)
 train_logdir = os.path.join(prm.logdir, "train",time.strftime("%m_%d_%H_%M_%S"))
 
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
 
-    #train_summary_writer = tf.summary.FileWriter(train_logdir, sess.graph)
+    train_summary_writer = tf.summary.FileWriter(train_logdir, sess.graph)
 
     test_x = np.zeros((prm.batch_size, prm.ip_h, prm.ip_w, prm.n_chan),dtype=float)
-    dims_up = sess.run([down_layers[1],up_layers[2]], feed_dict={ X:test_x})
+    dims_up = sess.run([down_layers[3],up_layers[3],final_out], feed_dict={ X:test_x})
